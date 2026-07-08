c1f87388e9ffba9b1c296c238aa5207e2171f8ca
Ali Garba <garbaali226@gmail.com>
2026-07-08 18:24:10 +0000

    Sécurisation de la connexion avec getenv

--- db.php
+++ db.php
@@ -1,16 +1,19 @@
 <?php
-$host = 'bibliotheque-project-bibliotheque-project.b.aivencloud.com/'; // Ton Host Aiven
-$port = '22347';                      // Ton Port Aiven
-$db   = 'defaultdb';                  // Ta base Aiven
-$user = 'avnadmin';                   // Ton User Aiven
-$pass = 'AVNS_ecQ4RNV5ALkevEm-8Et';     // Ton MDP Aiven
-$charset = 'utf8mb4';
+$host = 'bibliotheque-project-bibliotheque-project.b.aivencloud.com/'; // Ton host Aiven
+$port = '22347';                      // Ton port Aiven
+$db   = 'defaultdb';                  
+$user = 'avnadmin';                   
 
+// Cette ligne va chercher le mot de passe de manière sécurisée sans l'afficher
+$pass = getenv('DB_PASSWORD'); 
+
+$charset = 'utf8mb4';
 $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+
 try {
      $pdo = new PDO($dsn, $user, $pass);
-     echo "Connecté à Aiven avec succès !";
+     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (\PDOException $e) {
-     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+     die("Erreur de connexion : " . $e->getMessage());
 }
 ?>
