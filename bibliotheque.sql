6e36334f12868ef1fa0b08b87d100f0e6d0aa874
Ali Garba <garbaali226@gmail.com>
2026-07-08 18:17:01 +0000

    Mise à jour de la page db.php

--- bibliotheque.sql
+++ bibliotheque.sql
@@ -1,3 +1,4 @@
+SET SESSION sql_require_primary_key = 0;
 -- phpMyAdmin SQL Dump
 -- version 5.2.3
 -- https://www.phpmyadmin.net/
@@ -10,7 +11,6 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -28,11 +28,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `lecteurs` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `nom` varchar(100) NOT NULL,
   `prenom` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci, AUTO_INCREMENT=2;
 
 --
 -- Dumping data for table `lecteurs`
@@ -44,30 +46,18 @@ INSERT INTO `lecteurs` (`id`, `nom`, `prenom`, `email`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `liste_lecture`
---
-
-CREATE TABLE `liste_lecture` (
-  `id_livre` int NOT NULL,
-  `id_lecteur` int NOT NULL,
-  `date_emprunt` date DEFAULT NULL,
-  `date_retour` date DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `livres`
 --
 
 CREATE TABLE `livres` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `titre` varchar(100) NOT NULL,
   `auteur` varchar(100) NOT NULL,
   `description` text,
   `maison_edition` varchar(100) DEFAULT NULL,
-  `nombre_exemplaire` int DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `nombre_exemplaire` int DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci, AUTO_INCREMENT=5;
 
 --
 -- Dumping data for table `livres`
@@ -78,45 +68,20 @@ INSERT INTO `livres` (`id`, `titre`, `auteur`, `description`, `maison_edition`, 
 (2, 'Le Petit Prince', 'Antoine de Saint-Exupéry', 'Un conte poétique et philosophique.', 'Gallimard', 3),
 (3, '1984', 'George Orwell', 'Un roman de science-fiction dystopique.', 'Seuil', 2);
 
---
--- Indexes for dumped tables
---
+-- --------------------------------------------------------
 
 --
--- Indexes for table `lecteurs`
---
-ALTER TABLE `lecteurs`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- Indexes for table `liste_lecture`
---
-ALTER TABLE `liste_lecture`
-  ADD PRIMARY KEY (`id_livre`,`id_lecteur`),
-  ADD KEY `id_lecteur` (`id_lecteur`);
-
---
--- Indexes for table `livres`
---
-ALTER TABLE `livres`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- Table structure for table `liste_lecture`
 --
 
---
--- AUTO_INCREMENT for table `lecteurs`
---
-ALTER TABLE `lecteurs`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `livres`
---
-ALTER TABLE `livres`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+CREATE TABLE `liste_lecture` (
+  `id_livre` int NOT NULL,
+  `id_lecteur` int NOT NULL,
+  `date_emprunt` date DEFAULT NULL,
+  `date_retour` date DEFAULT NULL,
+  PRIMARY KEY (`id_livre`,`id_lecteur`),
+  KEY `id_lecteur` (`id_lecteur`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Constraints for dumped tables
@@ -128,6 +93,7 @@ ALTER TABLE `livres`
 ALTER TABLE `liste_lecture`
   ADD CONSTRAINT `liste_lecture_ibfk_1` FOREIGN KEY (`id_livre`) REFERENCES `livres` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `liste_lecture_ibfk_2` FOREIGN KEY (`id_lecteur`) REFERENCES `lecteurs` (`id`) ON DELETE CASCADE;
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- db.php
+++ db.php
@@ -1,13 +1,16 @@
 <?php
-$host = 'localhost';
-$dbname = 'bibliotheque';
-$username = 'root'; 
-$password = '';
+$host = 'bibliotheque-project-bibliotheque-project.b.aivencloud.com/'; // Ton Host Aiven
+$port = '22347';                      // Ton Port Aiven
+$db   = 'defaultdb';                  // Ta base Aiven
+$user = 'avnadmin';                   // Ton User Aiven
+$pass = 'AVNS_ecQ4RNV5ALkevEm-8Et';     // Ton MDP Aiven
+$charset = 'utf8mb4';
 
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
+     $pdo = new PDO($dsn, $user, $pass);
+     echo "Connecté à Aiven avec succès !";
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
 ?>
